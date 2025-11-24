5624d97d54013973107d655e6d14c88434451ee2
Adam Sherman <adam@sherman.ca>
2025-11-24 13:38:53 -0500

    Update diagnostics.py with diagnostics support code

---
+++ custom_components/rixens/diagnostics.py
@@ -0,0 +1,22 @@
+"""Diagnostics support."""
+from __future__ import annotations
+
+from homeassistant.components.diagnostics import async_redact_data
+from homeassistant.config_entries import ConfigEntry
+from homeassistant.core import HomeAssistant
+
+from .const import DOMAIN
+
+TO_REDACT = {"infra_ip", "key", "ssid"}
+
+
+async def async_get_config_entry_diagnostics(hass: HomeAssistant, entry: ConfigEntry):
+    coordinator = hass.data[DOMAIN][entry.entry_id]
+    return {
+        "entry": {
+            "title": entry.title,
+            "data": async_redact_data(entry.data, TO_REDACT),
+            "options": entry.options,
+        },
+        "latest_status": async_redact_data(coordinator.data, TO_REDACT),
+    }
