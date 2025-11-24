70aa2fdb7ed619b75e00f46ce7c85fd0896deb34
Adam Sherman <adam@sherman.ca>
2025-11-24 14:12:26 -0500

    Add basic config flow test

--- tests/test_config_flow.py
+++ tests/test_config_flow.py
@@ -6,10 +6,16 @@ from homeassistant.data_entry_flow import FlowResultType
 
 from custom_components.rixens.const import DOMAIN, CONF_HOST
 
+
 @pytest.mark.asyncio
-aSync def test_user_flow(hass: HomeAssistant):
-    result = await hass.config_entries.flow.async_init(DOMAIN, context={"source": config_entries.SOURCE_USER})
+async def test_user_flow(hass: HomeAssistant):
+    result = await hass.config_entries.flow.async_init(
+        DOMAIN, context={"source": config_entries.SOURCE_USER}
+    )
     assert result["type"] == FlowResultType.FORM
-    result2 = await hass.config_entries.flow.async_configure(result["flow_id"], user_input={CONF_HOST: "10.0.22.6"},)
+
+    result2 = await hass.config_entries.flow.async_configure(
+        result["flow_id"], user_input={CONF_HOST: "10.0.22.6"}
+    )
     assert result2["type"] == FlowResultType.CREATE_ENTRY
     assert result2["data"][CONF_HOST] == "10.0.22.6"
